a1b9e5417f1b31279df93d9314631e50eedfd972
Adam Hassan <adamhas132@gmail.com>
2024-11-16 17:02:28 -0600

    added setup

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+import dotenv from 'dotenv';
 import express from "express";
+import cors from 'cors';
+import bcrypt from 'bcrypt';
+import bodyParser from 'body-parser';
+import jwt from 'jsonwebtoken';
 
+// middleware setup
 const app = express();
-const port = 3000;
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
 
+const port = process.env.PORT;
+
+// start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// base endpoint
 app.get('/', (req, res) => {
   res.send('SemPlan server is up.');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+
