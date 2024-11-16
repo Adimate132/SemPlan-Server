c9f5d6daefc662616ecace13713a4d0f952cfa98
Adam Hassan <adamhas132@gmail.com>
2024-11-16 17:23:43 -0600

    added pinata setup

--- app.js
+++ app.js
@@ -4,15 +4,25 @@ import cors from 'cors';
 import bcrypt from 'bcrypt';
 import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken';
+dotenv.config(); // configure env vars immediately
+
+// pinata setup ----
+import { PinataSDK } from 'pinata';
+import fs from 'fs';
+import { Blob } from 'buffer';
+
+const pinata = new PinataSDK({
+  pinataJwt: process.env.PINATA_JWT,
+  pinataGateway: process.env.GATEWAY_URL
+})
+// ------------------
 
 // middleware setup
 const app = express();
-dotenv.config();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
-
-const port = process.env.PORT;
+const port = process.env.PORT; // PORT
 
 // start server
 app.listen(port, () => {
@@ -24,5 +34,8 @@ app.get('/', (req, res) => {
   res.send('SemPlan server is up.');
 });
 
+// createAcount 
+
+// verifyLogin
 
 
